0f94f6c4dccd8a0fdf6327189b8a10c0de368cd7
pinballtec <daniilboiko99@gmail.com>
2025-03-18 22:22:08 +0100

    fix ec2 without public ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -243,6 +243,7 @@ resource "aws_instance" "ocr_client" {
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   key_name               = "deploy-key-terraform-test"
+  associate_public_ip_address = true
 
   user_data = <<-EOF
     #!/bin/bash
