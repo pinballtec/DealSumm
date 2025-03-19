b7079a5af43e0521f500ea135d1950921086d9b9
pinballtec <daniilboiko99@gmail.com>
2025-03-19 19:27:13 +0100

    fix python file for ec2 machine

--- send_ocr_request.py
+++ send_ocr_request.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 import os
+import time  
 
 sqs = boto3.client('sqs', region_name=os.getenv('AWS_REGION'))
 queue_url = os.environ['SQS_QUEUE_URL']
@@ -17,6 +18,10 @@ def send_pdf_for_ocr(pdf_url, output_location):
     print(f'Message ID: {response["MessageId"]}')
 
 if __name__ == "__main__":
-    pdf_url = "s3://mybucket/input/output.pdf"
-    output_location = "s3://mybucket/output/output.pdf"
-    send_pdf_for_ocr(pdf_url, output_location)
+    while True:  
+        pdf_url = "s3://mybucket/input/output.pdf"
+        output_location = "s3://mybucket/output/output.pdf"
+        send_pdf_for_ocr(pdf_url, output_location)
+        
+        print("waiting before second sending")
+        time.sleep(10)  
